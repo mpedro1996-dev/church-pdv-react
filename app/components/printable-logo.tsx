bc239916493fdd04d66f852c2d1fa48eed75b3c1
pedro <pedrommoura1996@gmail.com>
2025-03-01 02:29:54 -0300

    Compilação com Tauri

--- app/components/printable-logo.tsx
+++ app/components/printable-logo.tsx
@@ -1,9 +1,7 @@
-import Image from 'next/image';
-
 export default function PrintableLogo(){
     return(
         <div className="flex justify-center items-center flex-col mb-2">
-            <Image src="/legado.jpg" alt="Imagem Fixa" width={200} height={100} className="max-w-full h-auto"/>
+            <img src="/legado.jpg" alt="Imagem Fixa" width={200} height={100} className="max-w-full h-auto"/>
         </div>
     )
 }
--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,9 @@ import 'dotenv/config';
 const nextConfig = {    
     env:{
         API_URL: process.env.API_URL
-    }
+    },
+    output: 'export',
+    distDir: 'out',
 };
 
 export default nextConfig;
